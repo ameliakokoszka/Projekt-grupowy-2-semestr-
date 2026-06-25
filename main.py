a51060af1d361ba15e5244da961ea702085a5784
skorupsk1 <oloskor@gmail.com>
2026-06-25 19:02:50 +0200

    Remove unnecessary blank lines in main.py

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ from source.gui import KalkulatorPunktow
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     app.setStyle("Fusion")
-    
     okno = KalkulatorPunktow()
     okno.show()
-    
     sys.exit(app.exec())
