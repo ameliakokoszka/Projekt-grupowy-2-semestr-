71342d98db8cd540467e0bec10f9fa4e80a871ab
skorupsk1 <oloskor@gmail.com>
2026-06-25 18:18:14 +0200

    Update import path for KalkulatorPunktow

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 from PySide6.QtWidgets import QApplication
 
-from src.gui import KalkulatorPunktow
+from source.gui import KalkulatorPunktow
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
